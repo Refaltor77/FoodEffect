1e9a6d54055027d99b5c3f7614254b63b6ae1d59
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-07-20 10:18:56 +0200

    Create FoodEffect.php

---
+++ src/refaltor/FoddEffect/event/FoodEffect.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FoodEffect\event;
+
+use FoodEffect\Main;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+
+class FoodEffect implements Listener
+{
+    public function onItemConsumed(PlayerItemConsumeEvent $event){
+        $player = $event->getPlayer();
+        $getAll = Main::getMain()->getConfig()->getAll();
+        $item = $event->getItem();
+        if ($player instanceof Player){
+            $inConfigIds = array_keys($getAll);
+            if (in_array($item->getId(), $inConfigIds)){
+                $food = Main::getMain()->getConfig()->getAll()[$item->getId()];
+                $effect = $food["effect"];
+                foreach($effect as $id => $values){
+                    $player->addEffect(new EffectInstance(Effect::getEffect($id), $values["duration"], $values["niveau"], $values["visible"]));
+                }
+            }
+        }
+    }
+}
